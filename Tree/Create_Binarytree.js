41918c1615572ad86a9191a316fa147f42fa825d
WatWittawat <wittawat.pho@ku.th>
2023-12-20 19:24:35 +0700

    uopdate

---
+++ Tree/Create_Binarytree.js
@@ -0,0 +1,101 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (currentNode.value > value) {
+        currentNode = currentNode.left;
+      } else if (currentNode.right > value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
+
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let tmp_parent = null;
+    while (currentNode) {
+      if (value < currentNode.left) {
+        tmp_parent = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.right) {
+        tmp_parent = currentNode;
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        // 3 Condition
+        if (currentNode.right === null) {
+          if (tmp_parent === null) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < tmp_parent.value) {
+              tmp_parent.left = currentNode.left;
+            } else if (currentNode.value > tmp_parent.value) {
+              tmp_parent.right = currentNode.left;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(10);
+tree.insert(4);
+tree.insert(12);
+tree.insert(13);
+tree.insert(20);
+console.log(tree.lookup(4));
+
+JSON.stringify(traverse(tree.root));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
